11aab6e1193b3bde50c8473273efa023da247a5e
txf0990 <txf0990@gmail.com>
2018-12-07 14:20:00 +1100

    Simplify Adder_2 cpp code.

--- src/chip/adder.cpp
+++ src/chip/adder.cpp
@@ -29,6 +29,7 @@ void Adder::CreateChip(pin_board::PinBoard& mother, const std::vector<PinIndex>&
     }
 }
 
+
 void Adder_2::CreateChip(pin_board::PinBoard& mother, const std::vector<PinIndex>& input1_pins, const std::vector<PinIndex>& input2_pins, const PinIndex carry_pin, const std::vector<PinIndex>& output_pins) {
     assert(input1_pins.size() == input2_pins.size());
     // input_pins = {low_dig, high_dig}
@@ -36,7 +37,7 @@ void Adder_2::CreateChip(pin_board::PinBoard& mother, const std::vector<PinIndex
     // if we want to calcualte 01+10, then the input is {1,0},{0,1}. output is {1,1,0}
     // if we want to calculate 11+11, then input {1,1},{1,1}, output is {0,1,1}
     if (input1_pins.size() == 1) {
-//        Adder::CreateChip(mother, {input1_pins[0], input2_pins[0], carry_pin}, output_pins);
+//        Adder_2::CreateChip(mother, {input1_pins[0], input2_pins[0], carry_pin}, output_pins);
         PinIndex Xor1_o = mother.AllocatePin();
         PinIndex Xor2_o = mother.AllocatePin();
         PinIndex And1_o = mother.AllocatePin();
@@ -49,7 +50,31 @@ void Adder_2::CreateChip(pin_board::PinBoard& mother, const std::vector<PinIndex
         OrGate::CreateChip(mother, {input1_pins[0], input2_pins[0]}, {Or1_o});
         AndGate::CreateChip(mother, {Or1_o, carry_pin}, {And2_o});
         OrGate::CreateChip(mother, {And1_o,And2_o}, {output_pins[1]});
-    } else if (input1_pins.size() == 2) { 
+    } else {
+        int digit = input1_pins.size();
+        PinIndex Adder_o = mother.AllocatePin();
+        std::vector<PinIndex> output = GetVectorPart(0, digit / 2, output_pins);
+        output.push_back(Adder_o);
+        Adder_2::CreateChip(
+                mother, 
+                GetVectorPart(0, digit / 2, input1_pins),
+                GetVectorPart(0, digit / 2, input2_pins),
+                carry_pin, 
+                output
+                );
+        output.clear();
+        output = GetVectorPart(digit / 2, digit / 2 + 1, output_pins);
+        Adder_2::CreateChip(
+                mother,
+                GetVectorPart(digit / 2, digit / 2, input1_pins),
+                GetVectorPart(digit / 2, digit / 2, input2_pins),
+                Adder_o,
+                output
+                );
+    }
+    
+/*
+    else if (input1_pins.size() == 2) { 
         PinIndex Adder_o = mother.AllocatePin();
         Adder_2::CreateChip(mother, {input1_pins[0]}, {input2_pins[0]}, carry_pin, {output_pins[0], Adder_o});
         Adder_2::CreateChip(mother, {input1_pins[1]}, {input2_pins[1]}, Adder_o, {output_pins[1], output_pins[2]});
@@ -118,6 +143,8 @@ void Adder_2::CreateChip(pin_board::PinBoard& mother, const std::vector<PinIndex
                 output
                 );
     }
+
+    */
 }
 
 }  //  namespace chip
